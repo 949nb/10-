80a319426f141086f61e69d62290b3322105a8fe
hy949 <1661346771@qq.com>
2019-10-19 22:36:15 +0800

    优化代码 处理请求错误

--- HTTP_SERVER/index.ts
+++ HTTP_SERVER/index.ts
@@ -2,35 +2,25 @@ import * as http from 'http'
 import * as fs from 'fs'
 import * as p from 'path'
 const publicDir = p.resolve(__dirname, 'public')
+import * as url from 'url'
 
 const server = http.createServer()
 
 server.on('request', (request: http.IncomingMessage, response:http.ServerResponse) => {
-  const {method, url} = request
-  switch (url) {
-    case '/index.html':
-      fs.readFile(p.resolve(publicDir, 'index.html'), (err, data) => {
-        if(err) throw err;
-        response.end(data.toString())
-      })
-      break;
-    case '/style.css' :
-      response.setHeader('Centent-Type', 'text/css; charset=utf-8')
-      fs.readFile(p.resolve(publicDir, 'style.css'), (err, data) => {
-        if(err) throw err
-        response.end(data.toString())
-      })
-      break;
-    case '/main.js' :
-      response.setHeader('Centent-Type', 'text/javascript; charset=utf-8')
-      fs.readFile(p.resolve(publicDir, 'main.js'), (err, data) => {
-        if(err) throw err
-        response.end(data.toString())
-      })
-      break;
-    default:
-      break;
-  }
+  const {method, url: path} = request
+  const { pathname, search } = url.parse(path)
+
+  const fileName = pathname.slice(1)
+  const fileSuffix = pathname.split('.')[1]
+  response.setHeader(`Centent-Type`, `text/${fileSuffix}; charset=utf-8`)
+  fs.readFile(p.resolve(publicDir, fileName), (err, data) => {
+    if(err) {
+      response.statusCode = 404
+      response.end()
+    } else {
+      response.end(data.toString())
+    }
+  })
 })
 
 server.listen(8888)
